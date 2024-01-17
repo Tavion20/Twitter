685a99a115c2b33799021eb7ea9c15c20a0bbea6
Tavion20 <tavionalric@gmail.com>
2024-01-17 21:36:50 +0530

    Placeholders Added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import Dummy from './dummy.json'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div style={{backgroundColor:'#B1B2FF',height:'3.5rem',width:'100%',marginBottom:'1rem'}}>Header Placeholder</div>
+      <div style={{display:'flex'}}>
+
+        <div style={{flex:3,padding:'3rem'}}>
+          <div>
+            Profile
+          </div>
+          <div style={{marginTop:'1rem',backgroundColor:'#EEF1FF',borderRadius:20}}>
+            <div style={{backgroundColor:'#B1B2FF',padding:'1rem',borderRadius:20}}>
+              Suggestion
+            </div>
+            <div>
+              
+            </div>
+          </div>
+        </div>
+
+
+        <div style={{flex:7}}>
+          Right
+        </div>
+      </div>
     </div>
   );
 }
